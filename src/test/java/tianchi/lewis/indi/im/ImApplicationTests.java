bd74f36e519e41797730ef4e0f16201ae5c29794
lewis <lingyidx@gmail.com>
2021-06-29 21:45:52 +0800

    init commit

---
+++ src/test/java/tianchi/lewis/indi/im/ImApplicationTests.java
@@ -0,0 +1,13 @@
+package tianchi.lewis.indi.im;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
