cd5993e0537e49c6fee297d4eb1b3bec323398a2
lewis <lingyidx@gmail.com>
2021-07-01 09:52:00 +0800

    feat: remove useless import

--- src/main/java/tianchi/lewis/indi/im/controller/MessageController.java
+++ src/main/java/tianchi/lewis/indi/im/controller/MessageController.java
@@ -1,7 +1,6 @@
 package tianchi.lewis.indi.im.controller;
 
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
--- src/main/java/tianchi/lewis/indi/im/controller/UserController.java
+++ src/main/java/tianchi/lewis/indi/im/controller/UserController.java
@@ -1,9 +1,6 @@
 package tianchi.lewis.indi.im.controller;
 
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
