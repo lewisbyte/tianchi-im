3b627398a96976cc6bbe84f5c28d3d28abdac022
lewis <lingyidx@gmail.com>
2021-07-04 16:47:33 +0800

    feat: 适配 sharding-jdbc

--- im-webapp/src/main/java/tianchi/lewis/indi/im/ImApplication.java
+++ im-webapp/src/main/java/tianchi/lewis/indi/im/ImApplication.java
@@ -4,13 +4,10 @@ import com.spring4all.swagger.EnableSwagger2Doc;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @EnableSwagger2Doc
-@EnableTransactionManagement(proxyTargetClass = true)
-@MapperScan("com.xkcoding.sharding.jdbc.mapper")
 @SpringBootApplication
 public class ImApplication {
 
