2efc31568bc20e307159677f8ee01f7afd479199
lewis <lingyidx@gmail.com>
2021-07-04 17:37:50 +0800

    feat: 适配 sharding-jdbc,依赖调整

--- im-datastore/src/main/java/tianchi/lewis/indi/im/serivce/impl/MessageDataStoreServiceImpl.java
+++ im-datastore/src/main/java/tianchi/lewis/indi/im/serivce/impl/MessageDataStoreServiceImpl.java
@@ -1,5 +1,7 @@
 package tianchi.lewis.indi.im.serivce.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import tianchi.lewis.indi.im.dao.MessageMapper;
 import tianchi.lewis.indi.im.serivce.MessageDataStoreService;
 
 /**
@@ -9,4 +11,10 @@ import tianchi.lewis.indi.im.serivce.MessageDataStoreService;
  * @create: 2021-07-03 12:04
  */
 public class MessageDataStoreServiceImpl implements MessageDataStoreService {
+    @Autowired
+    private MessageMapper mapper;
+
+    public void test(){
+    }
+
 }
