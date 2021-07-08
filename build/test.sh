80cde2326d0a6a46e191417164954b6f7fbafbe3
lewis <lingyidx@gmail.com>
2021-07-08 14:49:46 +0800

    fix: 测试脚本调整

--- build/test.sh
+++ build/test.sh
@@ -3,18 +3,17 @@ echo '\n --------------start test ------------------ \n';
 date;
 
 
-
 #room API 测试
 curl -X 'POST' \
   'http://localhost:8080/room' \
   -H 'accept: */*' \
-  -H 'Authorization: Bearer test_only_token' \
+  -H 'Authorization: Bearer 1412452156027965442' \
   -H 'Content-Type: application/json' \
   -d '{
   "name": "stringjjojojoojo"
 }';
 echo '\n 1#room \n';
-## 测试没有token场景
+# 测试没有token场景
 curl -X 'POST' \
   'http://localhost:8080/room' \
   -H 'accept: */*' \
@@ -25,42 +24,33 @@ curl -X 'POST' \
 echo '\n 2#room \n';
 
 
+# /userLogin
+curl -X 'GET' \
+  'http://localhost:8080/userLogin?username=djka01wq&password=string' \
+  -H 'accept: */*';
+echo '\n 11# /userLogin \n';
+
 
 
 #/room/{roomid}/enter
 curl -X 'PUT' \
-  'http://localhost:8080/room/test_only_room_id/enter' \
+  'http://localhost:8080/room/1412450112839880706/enter' \
   -H 'accept: */*' \
-  -H 'Authorization: Bearer test_only_token';
+  -H 'Authorization: Bearer 1412452156027965442';
 echo '\n  3#/room/{roomid}/enter \n';
 #/room/{roomid}/enter
 # 测试没有token场景
 curl -X 'PUT' \
-  'http://localhost:8080/room/test_only_room_id/enter' \
+  'http://localhost:8080/room/1412450112839880706/enter' \
   -H 'accept: */*' ;
 echo '\n  4#/room/{roomid}/enter \n';
 
 
 
 
-
-#/roomLeave
-curl -X 'PUT' \
-  'http://localhost:8080/roomLeave' \
-  -H 'accept: */*' \
-  -H 'Authorization: Bearer test_only_token';
-echo '\n 5#/roomLeave \n';
-#/roomLeave
-curl -X 'PUT' \
-  'http://localhost:8080/roomLeave' \
-  -H 'accept: */*' ;
-echo '\n 6#/roomLeave \n';
-
-
-
 #/room/{roomid}
 curl -X 'GET' \
-  'http://localhost:8080/room/test_only_room_id' \
+  'http://localhost:8080/room/1412450112839880706' \
   -H 'accept: */*';
 echo '\n 7#/room/{roomid} \n';
 
@@ -69,7 +59,7 @@ echo '\n 7#/room/{roomid} \n';
 
 #/room/{roomid}/users
 curl -X 'GET' \
-  'http://localhost:8080/room/test_only_room_id/users' \
+  'http://localhost:8080/room/1412450112839880706/users' \
   -H 'accept: application/json';
 echo '\n 8#/room/{roomid}/users \n';
 
@@ -97,23 +87,17 @@ curl -X 'POST' \
   -H 'accept: */*' \
   -H 'Content-Type: application/json' \
   -d '{
-  "username": "zhangsan",
-  "firstName": "zhangsan",
-  "lastName": "zhangsan",
-  "email": "zhangsan",
-  "password": "zhangsan",
-  "phone": "zhangsan"
+  "username": "1412452156027965442",
+  "firstName": "1412452156027965442",
+  "lastName": "1412452156027965442",
+  "email": "1412452156027965442",
+  "password": "1412452156027965442",
+  "phone": "1412452156027965442"
 }';
 echo '\n 10#/user \n';
 
 
 
-
-# /userLogin
-curl -X 'GET' \
-  'http://localhost:8080/userLogin?username=zhangsan&password=zhangsan' \
-  -H 'accept: */*';
-echo '\n 11# /userLogin \n';
 
 
 
@@ -133,7 +117,7 @@ echo '\n 12#/user/{username} \n';
 curl -X 'POST' \
   'http://localhost:8080/message/send' \
   -H 'accept: */*' \
-  -H 'Authorization: Bearer test_only_token' \
+  -H 'Authorization: Bearer 1412452156027965442' \
   -H 'Content-Type: application/json' \
   -d '{
   "id": "wobushixiaoxi",
@@ -158,7 +142,7 @@ echo '\n 14#/message/send \n';
 curl -X 'POST' \
   'http://localhost:8080/message/retrieve' \
   -H 'accept: application/json' \
-  -H 'Authorization: Bearer test_only_token' \
+  -H 'Authorization: Bearer 1412452156027965442' \
   -H 'Content-Type: application/json' \
   -d '{
   "pageIndex": -1,
@@ -175,6 +159,19 @@ curl -X 'POST' \
 }';
 echo '\n 16#/message/retrieve \n';
 
+
+#/roomLeave
+curl -X 'PUT' \
+  'http://localhost:8080/roomLeave' \
+  -H 'accept: */*' \
+  -H 'Authorization: Bearer 1412452156027965442';
+echo '\n 5#/roomLeave \n';
+#/roomLeave
+curl -X 'PUT' \
+  'http://localhost:8080/roomLeave' \
+  -H 'accept: */*' ;
+echo '\n 6#/roomLeave \n';
+
+
+
 echo '\n --------------end test ------------------ \n';
-
-
