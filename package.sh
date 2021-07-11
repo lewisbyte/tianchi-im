793f67a79b66a01001a672599f95348340d7ca1a
lewis <lingyidx@gmail.com>
2021-07-11 17:05:46 +0800

    提交

--- package.sh
+++ package.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 mvn clean install -Dmaven.test.skip=true;
 
-cp /Users/lewis/repo/tianchi-im/im-webapp/target/im-webapp-0.0.1-SNAPSHOT.jar /Users/lewis/repo/tianchi-im/deploy_application/application/
+cp /Users/lewis/repo/tianchi-im/im-webapp/target/im-webapp-0.0.1-SNAPSHOT.jar /Users/lewis/repo/tianchi-im/deploy_application/
 
-cd /Users/lewis/repo/tianchi-im/deploy_application;
+cd /Users/lewis/repo/tianchi-im/im-webapp-0.0.1-SNAPSHOT.jar;
 
 zip -r application.zip application;
 
