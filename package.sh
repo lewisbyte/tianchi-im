aa8e071040b19474ab6f4cc01cbb4bf65c5d048b
lewis <lingyidx@gmail.com>
2021-07-11 18:14:34 +0800

    更新脚本

--- package.sh
+++ package.sh
@@ -7,8 +7,6 @@ cd /Users/lewis/repo/tianchi-im/deploy_application/;
 
 zip -r application.zip im-webapp-0.0.1-SNAPSHOT.jar;
 
-rm -rf /Users/lewis/repo/tianchi-im/deploy_application/im-webapp-0.0.1-SNAPSHOT.jar;
-
 zip -r deploy_application.zip ./*;
 
 mv deploy_application.zip /Users/lewis/Desktop/
