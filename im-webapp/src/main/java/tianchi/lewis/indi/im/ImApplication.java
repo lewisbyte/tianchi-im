3d2e87d8230748622651d63fec533d09b358077d
lewis <lingyidx@gmail.com>
2021-07-11 15:53:47 +0800

    提交

--- im-webapp/src/main/java/tianchi/lewis/indi/im/ImApplication.java
+++ im-webapp/src/main/java/tianchi/lewis/indi/im/ImApplication.java
@@ -4,7 +4,6 @@ import com.spring4all.swagger.EnableSwagger2Doc;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
