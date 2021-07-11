d4fa87a375aa546a0768ea32b8c52c3501f3787c
lewis <lingyidx@gmail.com>
2021-07-11 18:33:17 +0800

    Revert "更新脚本"
    This reverts commit 3873d69f

--- deploy_application/start.sh
+++ deploy_application/start.sh
@@ -40,8 +40,11 @@ function run() {
 }
 start() {
     echo "start ..."
-    nohup java -jar ${app_file_name} > /dev/null 2>&1 &
-    sleep 20;
+    nohup java -XX:+UseConcMarkSweepGC \
+    -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8 -Duser.timezone=Asia/Shanghai \
+    -XX:+HeapDumpOnOutOfMemoryError -jar ${app_file_name} \
+    --isJar=true > ./im.log 2>&1 &
+    sleep 30;
 }
 # shellcheck disable=SC2166
 if [ "$option" == 'deploy' ]
