06f473c3ee67c056d96fef3fb5a8fe7ab1968eaf
lewis <lingyidx@gmail.com>
2021-07-13 12:32:17 +0800

    init vert.x

--- im-datastore/src/main/java/tianchi/lewis/indi/im/dao/MessageMapper.java
+++
@@ -1,9 +0,0 @@
-package tianchi.lewis.indi.im.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.springframework.stereotype.Component;
-import tianchi.lewis.indi.im.entity.TMessage;
-
-@Component
-public interface MessageMapper extends BaseMapper<TMessage> {
-}
