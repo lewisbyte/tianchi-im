d488eac250b0e2ac17bff3499016cd83778257d3
lewis <lingyidx@gmail.com>
2021-07-20 00:22:42 +0800

    feat: 完善代码

--- src/main/java/starter/router/impl/UserRouter.java
+++ src/main/java/starter/router/impl/UserRouter.java
@@ -4,14 +4,11 @@ import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
-import io.vertx.sqlclient.Row;
-import io.vertx.sqlclient.RowSet;
 import io.vertx.sqlclient.Tuple;
 import starter.constants.HttpHeaderConstant;
 import starter.dao.PGSQLUtils;
-import starter.exception.ControllerException;
 import starter.router.RouterConf;
-import starter.utils.SessionUtils;
+import starter.utils.CacheUser;
 
 /**
  * @program: tianchi-im-vert.x
@@ -33,20 +30,26 @@ public class UserRouter implements RouterConf {
         router.post("/user").handler(ctx -> {
             HttpServerResponse response = ctx.response();
             JsonObject body = ctx.getBodyAsJson();
+            if (CacheUser.exist(body.getString("username"))) {
+                response.setStatusCode(400).end();
+                return;
+            }
             PGSQLUtils.getConnection().compose(sqlConnection ->
-                    sqlConnection.
-                            preparedQuery("INSERT INTO t_user (username, first_name,last_name,email,password,phone) VALUES ($1, $2, $3, $4, $5, $6)")
+                    sqlConnection.preparedQuery("INSERT INTO t_user (username, first_name,last_name,email,password,phone) VALUES ($1, $2, $3, $4, $5, $6)")
                             .execute(Tuple.of(body.getString("username"),
                                     body.getString("firstName"),
                                     body.getString("lastName"),
                                     body.getString("email"),
                                     body.getString("password"),
                                     body.getString("phone"))
-                            ).onComplete(ar -> sqlConnection.close()));
-            response.putHeader(HttpHeaderConstant.content_type, HttpHeaderConstant.text_plain);
-            response.end();
-        }).failureHandler(ctx -> {
-            ctx.response().setStatusCode(400).end("Sorry! Not today");
+                            ).onComplete(ar -> sqlConnection.close())
+            )
+                    .onFailure(event -> response.setStatusCode(400).end())
+                    .onSuccess(event -> {
+                                CacheUser.add(body.getString("username"), body.getString("password"));
+                                response.end();
+                            }
+                    );
         });
     }
 
@@ -57,6 +60,20 @@ public class UserRouter implements RouterConf {
             HttpServerRequest request = ctx.request();
             String username = request.getParam("username");
             String password = request.getParam("password");
+
+            if (password.equals(CacheUser.get(username))){
+                response.end();
+                return;
+            }
+            PGSQLUtils.getConnection().compose(
+                    sqlConnection -> sqlConnection.
+                            preparedQuery("select count(*) from t_user where username=$1 and password=$2").
+                            execute(Tuple.of(username, password)).
+                            onComplete(a -> sqlConnection.close())
+            ).onSuccess(event -> {
+                CacheUser.add(username,password);
+                response.end();
+            }).onFailure(event -> response.setStatusCode(400).end());
         });
     }
 
