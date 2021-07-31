fe4990c0e3b6ad5761e4a1fe55a0cb98b35d8f41
lewis <lingyidx@gmail.com>
2021-07-31 15:11:25 +0800

    feat: 脚本更新

--- db/public.sql
+++ db/public.sql
@@ -1,154 +1,321 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : pgsql
- Source Server Type    : PostgreSQL
- Source Server Version : 130003
- Source Host           : localhost:5432
- Source Catalog        : postgres
- Source Schema         : public
-
- Target Server Type    : PostgreSQL
- Target Server Version : 130003
- File Encoding         : 65001
-
- Date: 22/07/2021 01:14:18
-*/
-
-
--- ----------------------------
--- Sequence structure for t_message_id_seq
--- ----------------------------
-DROP SEQUENCE IF EXISTS "public"."t_message_id_seq";
-CREATE SEQUENCE "public"."t_message_id_seq"
-INCREMENT 1
-MINVALUE  1
-MAXVALUE 9223372036854775807
-START 1
-CACHE 1;
-ALTER SEQUENCE "public"."t_message_id_seq" OWNER TO "postgres";
-
--- ----------------------------
--- Sequence structure for t_room_id_seq
--- ----------------------------
-DROP SEQUENCE IF EXISTS "public"."t_room_id_seq";
-CREATE SEQUENCE "public"."t_room_id_seq"
-INCREMENT 1
-MINVALUE  1
-MAXVALUE 9223372036854775807
-START 1
-CACHE 1;
-ALTER SEQUENCE "public"."t_room_id_seq" OWNER TO "postgres";
-
--- ----------------------------
--- Sequence structure for t_user_id_seq
--- ----------------------------
-DROP SEQUENCE IF EXISTS "public"."t_user_id_seq";
-CREATE SEQUENCE "public"."t_user_id_seq"
-INCREMENT 1
-MINVALUE  1
-MAXVALUE 9223372036854775807
-START 1
-CACHE 1;
-ALTER SEQUENCE "public"."t_user_id_seq" OWNER TO "postgres";
-
--- ----------------------------
--- Table structure for t_message
--- ----------------------------
-DROP TABLE IF EXISTS "public"."t_message";
-CREATE TABLE "public"."t_message" (
-  "id" int8 NOT NULL DEFAULT nextval('t_message_id_seq'::regclass),
-  "text" varchar(200) COLLATE "pg_catalog"."default",
-  "roomid" int8,
-  "stamp" int8,
-  "mid" varchar(200) COLLATE "pg_catalog"."default"
-)
-;
-ALTER TABLE "public"."t_message" OWNER TO "postgres";
-
--- ----------------------------
--- Table structure for t_room
--- ----------------------------
-DROP TABLE IF EXISTS "public"."t_room";
-CREATE TABLE "public"."t_room" (
-  "id" int8 NOT NULL DEFAULT nextval('t_room_id_seq'::regclass),
-  "name" varchar(200) COLLATE "pg_catalog"."default"
-)
-;
-ALTER TABLE "public"."t_room" OWNER TO "postgres";
-
--- ----------------------------
--- Table structure for t_user
--- ----------------------------
-DROP TABLE IF EXISTS "public"."t_user";
-CREATE TABLE "public"."t_user" (
-  "id" int8 NOT NULL DEFAULT nextval('t_user_id_seq'::regclass),
-  "username" varchar(200) COLLATE "pg_catalog"."default",
-  "first_name" varchar(200) COLLATE "pg_catalog"."default",
-  "last_name" varchar(200) COLLATE "pg_catalog"."default",
-  "email" varchar(200) COLLATE "pg_catalog"."default",
-  "password" varchar(200) COLLATE "pg_catalog"."default",
-  "phone" varchar(200) COLLATE "pg_catalog"."default"
-)
-;
-ALTER TABLE "public"."t_user" OWNER TO "postgres";
-
--- ----------------------------
--- Alter sequences owned by
--- ----------------------------
-ALTER SEQUENCE "public"."t_message_id_seq"
-OWNED BY "public"."t_message"."id";
-SELECT setval('"public"."t_message_id_seq"', 13, true);
-
--- ----------------------------
--- Alter sequences owned by
--- ----------------------------
-ALTER SEQUENCE "public"."t_room_id_seq"
-OWNED BY "public"."t_room"."id";
-SELECT setval('"public"."t_room_id_seq"', 20, true);
-
--- ----------------------------
--- Alter sequences owned by
--- ----------------------------
-ALTER SEQUENCE "public"."t_user_id_seq"
-OWNED BY "public"."t_user"."id";
-SELECT setval('"public"."t_user_id_seq"', 12, true);
-
--- ----------------------------
--- Indexes structure for table t_message
--- ----------------------------
-CREATE INDEX "t_message_roomid" ON "public"."t_message" USING btree (
-  "roomid" "pg_catalog"."int8_ops" ASC NULLS LAST
+create table if not exists t_message
+(
+	id bigserial not null
+		constraint t_message_pkey
+			primary key,
+	text varchar(200),
+	roomid bigint,
+	stamp bigint,
+	mid varchar(200)
 );
 
--- ----------------------------
--- Primary Key structure for table t_message
--- ----------------------------
-ALTER TABLE "public"."t_message" ADD CONSTRAINT "t_message_pkey" PRIMARY KEY ("id");
+alter table t_message owner to postgres;
 
--- ----------------------------
--- Primary Key structure for table t_room
--- ----------------------------
-ALTER TABLE "public"."t_room" ADD CONSTRAINT "t_room_pkey" PRIMARY KEY ("id");
+create index if not exists t_message_roomid
+	on t_message (roomid);
 
--- ----------------------------
--- Indexes structure for table t_user
--- ----------------------------
-CREATE UNIQUE INDEX "t_user_idx1" ON "public"."t_user" USING btree (
-  "username" COLLATE "pg_catalog"."default" "pg_catalog"."text_ops" ASC NULLS LAST,
-  "password" COLLATE "pg_catalog"."default" "pg_catalog"."text_ops" ASC NULLS LAST
-);
-CREATE UNIQUE INDEX "t_user_idx2" ON "public"."t_user" USING btree (
-  "username" COLLATE "pg_catalog"."default" "pg_catalog"."text_ops" ASC NULLS LAST,
-  "password" COLLATE "pg_catalog"."default" "pg_catalog"."text_ops" ASC NULLS LAST
+create table if not exists t_room
+(
+	id bigserial not null
+		constraint t_room_pkey
+			primary key,
+	name varchar(200)
 );
 
--- ----------------------------
--- Uniques structure for table t_user
--- ----------------------------
-ALTER TABLE "public"."t_user" ADD CONSTRAINT "t_user_uk1" UNIQUE ("username");
+alter table t_room owner to postgres;
 
--- ----------------------------
--- Primary Key structure for table t_user
--- ----------------------------
-ALTER TABLE "public"."t_user" ADD CONSTRAINT "t_user_pkey" PRIMARY KEY ("id");
+create table if not exists t_user
+(
+	id bigserial not null
+		constraint t_user_pkey
+			primary key,
+	username varchar(200)
+		constraint t_user_uk1
+			unique,
+	first_name varchar(200),
+	last_name varchar(200),
+	email varchar(200),
+	password varchar(200),
+	phone varchar(200)
+);
+
+alter table t_user owner to postgres;
+
+create unique index if not exists t_user_idx1
+	on t_user (username, password);
+
+create unique index if not exists t_user_idx2
+	on t_user (username, password);
+
+create table if not exists t_message_0
+(
+	id bigserial not null
+		constraint t_message_0_pkey
+			primary key,
+	text varchar(200),
+	roomid bigint,
+	stamp bigint,
+	mid varchar(200)
+);
+
+alter table t_message_0 owner to postgres;
+
+create index if not exists t_message_0_roomid
+	on t_message_0 (roomid);
+
+create table if not exists t_message_1
+(
+	id bigserial not null
+		constraint t_message_1_pkey
+			primary key,
+	text varchar(200),
+	roomid bigint,
+	stamp bigint,
+	mid varchar(200)
+);
+
+alter table t_message_1 owner to postgres;
+
+create index if not exists t_message_1_roomid
+	on t_message_1 (roomid);
+
+create table if not exists t_message_2
+(
+	id bigserial not null
+		constraint t_message_2_pkey
+			primary key,
+	text varchar(200),
+	roomid bigint,
+	stamp bigint,
+	mid varchar(200)
+);
+
+alter table t_message_2 owner to postgres;
+
+create index if not exists t_message_2_roomid
+	on t_message_2 (roomid);
+
+create table if not exists t_message_3
+(
+	id bigserial not null
+		constraint t_message_3_pkey
+			primary key,
+	text varchar(200),
+	roomid bigint,
+	stamp bigint,
+	mid varchar(200)
+);
+
+alter table t_message_3 owner to postgres;
+
+create index if not exists t_message_3_roomid
+	on t_message_3 (roomid);
+
+create table if not exists t_message_4
+(
+	id bigserial not null
+		constraint t_message_4_pkey
+			primary key,
+	text varchar(200),
+	roomid bigint,
+	stamp bigint,
+	mid varchar(200)
+);
+
+alter table t_message_4 owner to postgres;
+
+create index if not exists t_message_4_roomid
+	on t_message_4 (roomid);
+
+create table if not exists t_message_5
+(
+	id bigserial not null
+		constraint t_message_5_pkey
+			primary key,
+	text varchar(200),
+	roomid bigint,
+	stamp bigint,
+	mid varchar(200)
+);
+
+alter table t_message_5 owner to postgres;
+
+create index if not exists t_message_5_roomid
+	on t_message_5 (roomid);
+
+create table if not exists t_message_6
+(
+	id bigserial not null
+		constraint t_message_6_pkey
+			primary key,
+	text varchar(200),
+	roomid bigint,
+	stamp bigint,
+	mid varchar(200)
+);
+
+alter table t_message_6 owner to postgres;
+
+create index if not exists t_message_6_roomid
+	on t_message_6 (roomid);
+
+create table if not exists t_message_7
+(
+	id bigserial not null
+		constraint t_message_7_pkey
+			primary key,
+	text varchar(200),
+	roomid bigint,
+	stamp bigint,
+	mid varchar(200)
+);
+
+alter table t_message_7 owner to postgres;
+
+create index if not exists t_message_7_roomid
+	on t_message_7 (roomid);
+
+create table if not exists t_message_8
+(
+	id bigserial not null
+		constraint t_message_8_pkey
+			primary key,
+	text varchar(200),
+	roomid bigint,
+	stamp bigint,
+	mid varchar(200)
+);
+
+alter table t_message_8 owner to postgres;
+
+create index if not exists t_message_8_roomid
+	on t_message_8 (roomid);
+
+create table if not exists t_message_9
+(
+	id bigserial not null
+		constraint t_message_9_pkey
+			primary key,
+	text varchar(200),
+	roomid bigint,
+	stamp bigint,
+	mid varchar(200)
+);
+
+alter table t_message_9 owner to postgres;
+
+create index if not exists t_message_9_roomid
+	on t_message_9 (roomid);
+
+create table if not exists t_message_10
+(
+	id bigserial not null
+		constraint t_message_10_pkey
+			primary key,
+	text varchar(200),
+	roomid bigint,
+	stamp bigint,
+	mid varchar(200)
+);
+
+alter table t_message_10 owner to postgres;
+
+create index if not exists t_message_10_roomid
+	on t_message_10 (roomid);
+
+create table if not exists t_message_11
+(
+	id bigserial not null
+		constraint t_message_11_pkey
+			primary key,
+	text varchar(200),
+	roomid bigint,
+	stamp bigint,
+	mid varchar(200)
+);
+
+alter table t_message_11 owner to postgres;
+
+create index if not exists t_message_11_roomid
+	on t_message_11 (roomid);
+
+create table if not exists t_message_12
+(
+	id bigserial not null
+		constraint t_message_12_pkey
+			primary key,
+	text varchar(200),
+	roomid bigint,
+	stamp bigint,
+	mid varchar(200)
+);
+
+alter table t_message_12 owner to postgres;
+
+create index if not exists t_message_12_roomid
+	on t_message_12 (roomid);
+
+create table if not exists t_message_13
+(
+	id bigserial not null
+		constraint t_message_13_pkey
+			primary key,
+	text varchar(200),
+	roomid bigint,
+	stamp bigint,
+	mid varchar(200)
+);
+
+alter table t_message_13 owner to postgres;
+
+create index if not exists t_message_13_roomid
+	on t_message_13 (roomid);
+
+create table if not exists t_message_14
+(
+	id bigserial not null
+		constraint t_message_14_pkey
+			primary key,
+	text varchar(200),
+	roomid bigint,
+	stamp bigint,
+	mid varchar(200)
+);
+
+alter table t_message_14 owner to postgres;
+
+create index if not exists t_message_14_roomid
+	on t_message_14 (roomid);
+
+create table if not exists t_message_15
+(
+	id bigserial not null
+		constraint t_message_15_pkey
+			primary key,
+	text varchar(200),
+	roomid bigint,
+	stamp bigint,
+	mid varchar(200)
+);
+
+alter table t_message_15 owner to postgres;
+
+create index if not exists t_message_15_roomid
+	on t_message_15 (roomid);
+
+create table if not exists t_message_16
+(
+	id bigserial not null
+		constraint t_message_16_pkey
+			primary key,
+	text varchar(200),
+	roomid bigint,
+	stamp bigint,
+	mid varchar(200)
+);
+
+alter table t_message_16 owner to postgres;
+
+create index if not exists t_message_16_roomid
+	on t_message_16 (roomid);
+
