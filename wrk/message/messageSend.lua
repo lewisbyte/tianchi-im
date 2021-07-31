d585a77ed15dea74b8f2c7dc372535be0dd77f23
lewis <lingyidx@gmail.com>
2021-07-31 15:39:29 +0800

    feat: 更新压测场景

--- wrk/message/messageSend.lua
+++ wrk/message/messageSend.lua
@@ -1,4 +1,29 @@
 wrk.method = "POST"
 wrk.headers["Content-Type"] =  "application/json"
-wrk.headers["Authorization"] =  "Bearer 6huihihuihiuh"
+wrk.headers["Authorization"] =  "Bearer 1"
+wrk.body = "{\"id\": \"test\",\"text\":\"dasdad12ffffff\"}"
+
+wrk.method = "POST"
+wrk.headers["Content-Type"] =  "application/json"
+wrk.headers["Authorization"] =  "Bearer 2"
+wrk.body = "{\"id\": \"test\",\"text\":\"dasdad12ffffff\"}"
+
+wrk.method = "POST"
+wrk.headers["Content-Type"] =  "application/json"
+wrk.headers["Authorization"] =  "Bearer 3"
+wrk.body = "{\"id\": \"test\",\"text\":\"dasdad12ffffff\"}"
+
+wrk.method = "POST"
+wrk.headers["Content-Type"] =  "application/json"
+wrk.headers["Authorization"] =  "Bearer 4"
+wrk.body = "{\"id\": \"test\",\"text\":\"dasdad12ffffff\"}"
+
+wrk.method = "POST"
+wrk.headers["Content-Type"] =  "application/json"
+wrk.headers["Authorization"] =  "Bearer 5"
+wrk.body = "{\"id\": \"test\",\"text\":\"dasdad12ffffff\"}"
+
+wrk.method = "POST"
+wrk.headers["Content-Type"] =  "application/json"
+wrk.headers["Authorization"] =  "Bearer 6"
 wrk.body = "{\"id\": \"test\",\"text\":\"dasdad12ffffff\"}"
