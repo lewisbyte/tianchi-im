23c967745f8d7f192213a2a334e5d3857b3cc0a4
lewis <lingyidx@gmail.com>
2021-07-31 14:33:14 +0800

    feat: 完善压测脚本

--- wrk/message/messageRetrieve.lua
+++ wrk/message/messageRetrieve.lua
@@ -1,4 +1,4 @@
 wrk.method = "POST"
-wrk.body   = "foo=bar&baz=quux"
 wrk.headers["Authorization"] =  "Bearer 6huihihuihiuh"
 wrk.headers["Content-Type"] = "application/x-www-form-urlencoded"
+wrk.body = "{\"pageIndex\": -1,\"pageSize\": 100}"
