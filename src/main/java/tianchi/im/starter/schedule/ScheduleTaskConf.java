444cbe936f7a645aebdfa3687fb3bca73a643ba4
lewis <lingyidx@gmail.com>
2021-08-02 20:49:47 +0800

    feat:  修复查询

--- src/main/java/tianchi/im/starter/schedule/ScheduleTaskConf.java
+++ src/main/java/tianchi/im/starter/schedule/ScheduleTaskConf.java
@@ -5,6 +5,7 @@ import tianchi.im.starter.dao.AsyncBatchInsertDao;
 
 public class ScheduleTaskConf {
     public static void conf(Vertx vertx) {
+        // 等待80ms ，用于积压一些数据量，产生批量提交的效果
         vertx.setPeriodic(80, (l) -> {
             AsyncBatchInsertDao.handleMessage();
         });
